070ad0da1470ea7f765e94443f1925a1f085b4da
vkrishn1Mortenson <vkrishn1mortenson@mortenson.com>
2026-08-04 14:13:11 -0500

    one more commit for the day to catch any updated files.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,7 +6,7 @@ import {
   CardItem,
 } from "@/components/ui/3d-card";
 import { Link } from 'react-router-dom';
-
+import pic3 from "@/assets/pic3.jpg";
 interface Project {
   project_id: string | null;
   project_code: string | null;
@@ -73,7 +73,12 @@ export default function Projects() {
   }
 
   return (
-    <div className="min-h-screen">
+    <div
+  className="min-h-screen bg-cover bg-center bg-no-repeat"
+  style={{
+    backgroundImage: `url(${pic3})`,
+  }}
+>
       <CardContainer className="inter-var">
         <CardBody className="relative h-auto w-auto rounded-xl border border-black/[0.1] bg-gray-50 p-6 group/card sm:w-[30rem] dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]">
           <CardItem
